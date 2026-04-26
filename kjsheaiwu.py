b5b226e6fd3d17e124d0a5984b8c734506b5770c
Marc25p <marcpasrib1@gmail.com>
2026-04-26 02:42:47 +0200

    borra

--- kjsheaiwu.py
+++ kjsheaiwu.py
@@ -1,10 +1,6 @@
-# To run this code you need to install the following dependencies:
-# pip install google-genai
-
 import os
 from google import genai
 from google.genai import types
-
 
 def generate():
     client = genai.Client(
